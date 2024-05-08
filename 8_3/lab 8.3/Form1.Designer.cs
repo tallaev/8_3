b169c49cac4de117e282d732d749abbb34e88d6e
tallaev <98256797+tallaev@users.noreply.github.com>
2024-05-08 15:48:37 +0700

    Add files via upload

---
+++ 8_3/lab 8.3/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace lab_8._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Prob1 = new System.Windows.Forms.TextBox();
+            this.Prob2 = new System.Windows.Forms.TextBox();
+            this.Prob3 = new System.Windows.Forms.TextBox();
+            this.Prob4 = new System.Windows.Forms.TextBox();
+            this.Prob5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.num = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Prob1
+            // 
+            this.Prob1.Location = new System.Drawing.Point(59, 16);
+            this.Prob1.Name = "Prob1";
+            this.Prob1.Size = new System.Drawing.Size(72, 20);
+            this.Prob1.TabIndex = 0;
+            // 
+            // Prob2
+            // 
+            this.Prob2.Location = new System.Drawing.Point(59, 42);
+            this.Prob2.Name = "Prob2";
+            this.Prob2.Size = new System.Drawing.Size(72, 20);
+            this.Prob2.TabIndex = 1;
+            // 
+            // Prob3
+            // 
+            this.Prob3.Location = new System.Drawing.Point(59, 68);
+            this.Prob3.Name = "Prob3";
+            this.Prob3.Size = new System.Drawing.Size(72, 20);
+            this.Prob3.TabIndex = 2;
+            // 
+            // Prob4
+            // 
+            this.Prob4.Location = new System.Drawing.Point(59, 94);
+            this.Prob4.Name = "Prob4";
+            this.Prob4.Size = new System.Drawing.Size(72, 20);
+            this.Prob4.TabIndex = 3;
+            // 
+            // Prob5
+            // 
+            this.Prob5.Location = new System.Drawing.Point(59, 120);
+            this.Prob5.Name = "Prob5";
+            this.Prob5.Size = new System.Drawing.Size(72, 20);
+            this.Prob5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Prob1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Prob2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Prob3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Prob4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Prob5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Number of experiments";
+            // 
+            // num
+            // 
+            this.num.Location = new System.Drawing.Point(137, 154);
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(72, 20);
+            this.num.TabIndex = 12;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(57, 194);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 13;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.num);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Prob5);
+            this.panel1.Controls.Add(this.Prob4);
+            this.panel1.Controls.Add(this.Prob3);
+            this.panel1.Controls.Add(this.Prob2);
+            this.panel1.Controls.Add(this.Prob1);
+            this.panel1.Location = new System.Drawing.Point(13, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(228, 227);
+            this.panel1.TabIndex = 14;
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(261, 15);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(586, 364);
+            this.chart.TabIndex = 15;
+            this.chart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 394);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Prob1;
+        private System.Windows.Forms.TextBox Prob2;
+        private System.Windows.Forms.TextBox Prob3;
+        private System.Windows.Forms.TextBox Prob4;
+        private System.Windows.Forms.TextBox Prob5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox num;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ 8_3/lab 8.3/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab_8._3
+{
+    public partial class Form1 : Form
+    {
+        private readonly Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int numTrials = int.Parse(num.Text);
+            double[] probabilities = CalculateProbabilities();
+
+            List<int> events = GenerateEvents(numTrials, probabilities);
+            DisplayEmpiricalDistribution(events);
+        }
+
+        private double[] CalculateProbabilities()
+        {
+            double[] probabilities = { double.Parse(Prob1.Text), double.Parse(Prob2.Text), double.Parse(Prob3.Text), double.Parse(Prob4.Text) };
+            double lastProbability = 1 - probabilities.Sum();
+            Prob5.Text = lastProbability.ToString();
+            return probabilities.Concat(new[] { lastProbability }).ToArray();
+        }
+
+        private void DisplayEmpiricalDistribution(List<int> events)
+        {
+            var eventCounts = events.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+            PlotChart(eventCounts);
+        }
+
+        private List<int> GenerateEvents(int numTrials, double[] probabilities)
+        {
+            var events = new List<int>();
+
+            for (int i = 0; i < numTrials; i++)
+                events.Add(GenerateEvent(probabilities));
+
+            return events;
+        }
+
+        private int GenerateEvent(double[] probabilities)
+        {
+            double rnd = random.NextDouble();
+            double cumulativeProbability = 0;
+
+            for (int i = 0; i < probabilities.Length; i++)
+            {
+                cumulativeProbability += probabilities[i];
+                if (rnd < cumulativeProbability)
+                    return i + 1;
+            }
+
+            return probabilities.Length;
+        }
+
+        private void PlotChart(Dictionary<int, int> eventCounts)
+        {
+            chart.Series.Clear();
+            Series series = new Series("Event Frequency") { ChartType = SeriesChartType.Column };
+
+            foreach (var kvp in eventCounts.OrderBy(x => x.Key))
+                series.Points.AddXY(kvp.Key, kvp.Value);
+
+            chart.Series.Add(series);
+            chart.ChartAreas[0].AxisX.Interval = 1;
+        }
+    }
+}
